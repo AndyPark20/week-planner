e59b26509e3e5045ba5a0fe523a89c238ea21038
Andy Park <apark1214@gmail.com>
2020-12-07 13:17:36 -0800

    completed unhiding modal

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
+var $addEntryBtn =document.querySelector('.button');
 var $userSubmit = document.querySelector('form');
-var $modal = document.querySelector('.modal')
+var $modal = document.querySelector('.modal');
 
-$userSubmit.addEventListener('submit', function(event){
 
-})
+function hideModal(e){
+
+  if (e.target.className ==='button'){
+    $modal.classList.remove('hidden');
+  }
+
+}
+
+$addEntryBtn.addEventListener('click', hideModal)
+
+
+
+// $userSubmit.addEventListener('submit', function(event){
+
+
+
+// })

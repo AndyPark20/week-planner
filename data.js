9ccf54b9c75a2edcb052d221ae67fcdaf4002936
josh choi <wonchoi1994@gmail.com>
2020-12-07 16:20:25 -0800

    input submit

--- data.js
+++ data.js
@@ -1,7 +1,9 @@
-var userEntry = [
-  {
-    weekday:'',
-    time: '',
-    info:''
-  }
-];
+var weekday = {
+  monday: { entries: { time: "", description: "" } },
+  tuesday: { entries: { time: "", description: "" } },
+  wednesday: { entries: { time: "", description: "" } },
+  thursday: { entries: { time: "", description: "" } },
+  friday: { entries: { time: "", description: "" } },
+  saturday: { entries: { time: "", description: "" } },
+  sunday: { entries: { time: "", description: "" } },
+};
--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 var $addEntryBtn = document.querySelector(".button");
 var $userSubmit = document.querySelector("form");
 var $modal = document.querySelector(".modal");
-var $spanDays = document.querySelector('.weekday-btn');
-var $h2Days = document.querySelector('#h2Days');
-
-
+var $spanDays = document.querySelector(".weekday-btn");
+var $h2Days = document.querySelector("#h2Days");
 
 function hideModal(e) {
   if (e.target.className === "button") {
@@ -14,33 +12,31 @@ function hideModal(e) {
 
 $addEntryBtn.addEventListener("click", hideModal);
 
-
-document.addEventListener('click',function(event){
-if(event.target.className === 'weekday-btn') {
-  console.log(event.target.getAttribute('data-view'));
-  $h2Days.textContent = ('Scheduled event for ' + event.target.getAttribute('data-view'));
-}
-})
-
-
-
-$userSubmit.addEventListener("submit", function (event) {
-  event.preventDefault();
-
+document.addEventListener("click", function (event) {
+  if (event.target.className === "weekday-btn") {
+    console.log(event.target.getAttribute("data-view"));
+    $h2Days.textContent =
+      "Scheduled event for " + event.target.getAttribute("data-view");
+  }
 });
 
+$userSubmit.addEventListener("submit", function (event) {
+  var $userWeekday = $userSubmit.elements.days.value;
+  console.log($userWeekday);
 
-function renderElements(object){
+  event.preventDefault();
+});
 
-var $row =document.createElement('tr');
-var $tableData =document.createElement('td');
-var $tableDataTwo =document.createElement('td');
-var $tablePosition = document.querySelector('.dataRow')
-// $tableData.textContent = 'Hello Cohort';
-$row.appendChild($tableData);
-// $tableDataTwo.textContent=""
-$row.appendChild($tableDataTwo);
-$tablePosition.appendChild($row);
+function renderElements(object) {
+  var $row = document.createElement("tr");
+  var $tableData = document.createElement("td");
+  var $tableDataTwo = document.createElement("td");
+  var $tablePosition = document.querySelector(".dataRow");
+  // $tableData.textContent = 'Hello Cohort';
+  $row.appendChild($tableData);
+  // $tableDataTwo.textContent=""
+  $row.appendChild($tableDataTwo);
+  $tablePosition.appendChild($row);
 
-return $tableData;
+  return $tableData;
 }

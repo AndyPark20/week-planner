80403e50c5371e56dc24d3ba272e49d5b6d25aa9
josh choi <wonchoi1994@gmail.com>
2020-12-07 17:52:04 -0800

    data pushed into table

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var $userSubmit = document.querySelector("form");
 var $modal = document.querySelector(".modal");
 var $spanDays = document.querySelector(".weekday-btn");
 var $h2Days = document.querySelector("#h2Days");
+var $tablePosition = document.querySelector(".dataRow");
 
 function hideModal(e) {
   if (e.target.className === "button") {
@@ -10,16 +11,22 @@ function hideModal(e) {
   }
 }
 
-
+function appendElements(entries) {
+  for (var i = 0; i < entries.length; i++) {
+    renderElements(entries[i]);
+  }
+}
 
 $addEntryBtn.addEventListener("click", hideModal);
 
 document.addEventListener("click", function (event) {
+  var userDay = event.target.getAttribute("data-view");
   if (event.target.className === "weekday-btn") {
-    console.log(event.target.getAttribute("data-view"));
+    // console.log(event.target.getAttribute("data-view"));
     $h2Days.textContent =
       "Scheduled event for " + event.target.getAttribute("data-view");
   }
+  appendElements(weekday[userDay].entries);
 });
 
 $userSubmit.addEventListener("submit", function (event) {
@@ -27,29 +34,23 @@ $userSubmit.addEventListener("submit", function (event) {
   var $userWeekday = $userSubmit.elements.days.value;
   var time = $userSubmit.elements.time.value;
   var description = $userSubmit.elements.description.value;
-  var entry = {time,description};
+  var entry = { time, description };
   weekday[$userWeekday].entries.push(entry);
   // renderElements(entry);
-  $modal.classList.add('hidden');
+  $modal.classList.add("hidden");
   $userSubmit.reset();
 });
 
-
 function renderElements(object) {
+  $tablePosition.textContent = "";
+
   var $row = document.createElement("tr");
   var $tableData = document.createElement("td");
   var $tableDataTwo = document.createElement("td");
-  var $tablePosition = document.querySelector(".dataRow");
+ 
   $tableData.textContent = object.time;
   $row.appendChild($tableData);
-  $tableDataTwo.textContent= object.description;
+  $tableDataTwo.textContent = object.description;
   $row.appendChild($tableDataTwo);
-  return $row;
-}
-
-function appendElements(array) {
-  for(var i = 0; i < array.length; i++ ) {
-    var position
-    array[i].
-  }
+  $tablePosition.appendChild($row);
 }
